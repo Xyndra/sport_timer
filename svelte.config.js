077d3c1c476e5bea097d063e187a34208a5278a6
Xyndra <71836523+Xyndra@users.noreply.github.com>
2025-05-06 18:03:13 +0200

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ out: 'build' }) }
+	kit: { adapter: adapter() }
 };
 
 export default config;
